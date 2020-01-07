6b7bb7f9a5936ab9129fa9cdf379d675db111341
John H <johnhayesru@gmail.com>
2020-01-08 01:31:56 +0300

    added landing page content and navbar components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
 import "./App.css";
 
 const App = () => {
   return (
-    <Fragment>
-      <h1>App</h1>
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+      </Fragment>
+    </Router>
   );
 };
 
