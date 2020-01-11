f05a4d73d92267af761ee6f482bcd39b071210a4
John H <johnhayesru@gmail.com>
2020-01-11 15:16:39 +0300

    changed all file extension to js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,11 @@ import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import "./App.css";
 
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
-
-import "./App.css";
 
 const App = () => {
   return (
