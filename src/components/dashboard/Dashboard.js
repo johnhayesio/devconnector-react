64d9a3504952324e62a4a2e09dea99b5c79cbee9
John H <johnhayesru@gmail.com>
2020-01-29 01:15:09 +0300

    added experience and education to user Dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import DashboardActions from "./DashboardActions";
+import Experience from "./Experience";
+import Education from "./Education";
 import Spinner from "../layout/Spinner";
 import { getCurrentProfile } from "../../actions/profile";
 
@@ -27,6 +29,8 @@ const Dashboard = ({
       {profile !== null ? (
         <Fragment>
           <DashboardActions />
+          <Experience experience={profile.experience} />
+          <Education education={profile.education} />
         </Fragment>
       ) : (
         <Fragment>
