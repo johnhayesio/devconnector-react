63715bae45882fdacae7f28ac571bd7fab454f1a
John H <johnhayesru@gmail.com>
2020-01-30 02:02:37 +0300

    implemented showing all developers using navlink

--- src/components/profiles/Profiles.js
+++ src/components/profiles/Profiles.js
@@ -2,17 +2,37 @@ import React, { Fragment, useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
+import ProfileItem from "../profiles/ProfileItem";
 import { getProfiles } from "../../actions/profile";
 
 const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
   useEffect(() => {
     getProfiles();
-    // eslint-ignore-next-line
-  }, []);
+  }, [getProfiles]);
+
   return (
-    <div>
-      <p>Hello</p>
-    </div>
+    <Fragment>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <Fragment>
+          <h1 className="large text-primary">Developers</h1>
+          <p className="lead">
+            <i className="fab fa-connectdevelop"></i> Browse and connect with
+            developers
+          </p>
+          <div className="profiles">
+            {profiles.length > 0 ? (
+              profiles.map(profile => (
+                <ProfileItem key={profile._id} profile={profile} />
+              ))
+            ) : (
+              <h4>No profiles found...</h4>
+            )}
+          </div>
+        </Fragment>
+      )}
+    </Fragment>
   );
 };
 
