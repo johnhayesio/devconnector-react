4ffce469e12dc2df0388c2efe6d7c21dad5b185a
John H <johnhayesru@gmail.com>
2020-01-30 01:12:01 +0300

    added Profiles component and imported to App.js

---
+++ src/components/profiles/Profiles.js
@@ -0,0 +1,24 @@
+import React, { Fragment, useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import Spinner from "../layout/Spinner";
+import { getProfiles } from "../../actions/profile";
+
+const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
+  useEffect(() => {
+    getProfiles();
+    // eslint-ignore-next-line
+  }, []);
+  return null;
+};
+
+Profiles.propTypes = {
+  getProfiles: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => {
+  profile: state.profile;
+};
+
+export default connect(mapStateToProps, { getProfiles })(Profiles);
