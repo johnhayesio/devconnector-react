70943aff16a19978443afb6ced10a466b8524a06
John H <johnhayesru@gmail.com>
2020-02-01 01:28:24 +0300

    added DELETE_POST functionality and fixed POST_ERROR

--- src/reducers/post.js
+++ src/reducers/post.js
@@ -31,7 +31,7 @@ export default function(state = initialState, action) {
     case POST_ERROR:
       return {
         ...state,
-        posts: payload,
+        error: payload,
         loading: false
       };
     case UPDATE_LIKES:
